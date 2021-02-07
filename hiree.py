774bf0b6f7b6a07fca6c0bb2d123a964d2f83393
Egor <egorius@MacBook-Air-Egor.local>
2021-02-08 02:20:12 +0300

    v1.1 Оптимизация

--- hiree.py
+++ hiree.py
@@ -2,7 +2,7 @@
 
 import requests
 from bs4 import BeautifulSoup
-import time
+from time import sleep
 import os
 from tqdm import tqdm
 # достает html код по указанной ссылке
@@ -10,17 +10,16 @@ from tqdm import tqdm
 local_proc = 0
 
 
-def get_html(url, f=True):
+def get_html(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    html = ""
     try:
-        rq = requests.get(url, headers=headers)
+        html = requests.get(url, headers=headers).text
     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
         print("Обработка исключения в get_html()...")
-        time.sleep(3)
-    if(f):
-        print("Getting HTML-code from ", url)
-    return rq.text
+        sleep(3)
+    return html
 
 
 # проверяет, есть ли на странице(глобальной) ссылки на вакансии
@@ -39,11 +38,11 @@ def is_empty(html):
 def get_resumes_links(html):
     # новый объект класса BeutifulSoup
     soup = BeautifulSoup(html, 'lxml')
-    new_links = []
+    new_links = ()
     links = soup.find_all('a', class_='resume-search-item__name')
     for link in links:
         link_parsed = ("http://hh.ru") + link.get('href')
-        new_links.append(link_parsed)
+        new_links += (link_parsed,)
     return new_links
 
 # функция, которая для данного запроса и региона ищет все страницы с результатами поиска и набирает большой список со всеми ссылками на вакансии
@@ -51,28 +50,30 @@ def get_resumes_links(html):
 
 
 def get_all_resumes_links(query, area, number_of_links):
+    print("Собираем глобальные ссылки...")
     # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    url_base = 'https://hh.ru/search/resume?clusters=True'
-    url_area = '&area='+area
-    url_base2 = '&order_by=relevance&logic=normal&pos=position&exp_period=all_time&no_magic=False&st=resumeSearch'
-    url_text = '&text='+query
-    url_page = '&page='
+    # url_base = 'https://hh.ru/search/resume?clusters=True'
+    # url_area = '&area='+area
+    # url_base2 = '&order_by=relevance&logic=normal&pos=position&exp_period=all_time&no_magic=False&st=resumeSearch'
+    # url_text = '&text='+query
+    # url_page = '&page='
 
+    basic_url = "".join(tuple(("https://hh.ru/search/resume?clusters=True", "&area=", area,
+                               "&order_by=relevance&logic=normal&pos=position&exp_period=all_time&no_magic=False&st=resumeSearch", "&text=", query, "&page=")))
     # когда не найдем с помощью bs4 нужный элемент, то выставим его False
     # нужен для остановки цикла перебора всех страниц
     # page_is_not_empty = True
 
-    all_links = []
+    all_links = ()
     # page = 1
 
-    for i in range(number_of_links//20):
-        url = url_base + url_area+url_base2 + url_text + url_page + str(i)
-        # time.sleep(.5)
+    for i in tqdm(range(number_of_links//20)):
+        url = basic_url+str(i)  # "".join(tuple((basic_url, str(i))))
         try:
             html = get_html(url)
         except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
             print(" Переподключение к глобальной странице...")
-            time.sleep(3)
+            sleep(3)
         all_links += get_resumes_links(html)
         '''
         if not is_empty(html):
@@ -107,18 +108,18 @@ def parse_exp_in_resume(soup, demanded_exp, procents, position):
     else:
         total_exp = int(s[2])
     # print("Общий стаж:", total_exp, "месяцев")
-    cur_exp = 0  # Опыт на одном из мест
-    s *= 0
+    # cur_exp = 0  # Опыт на одном из мест
+    # s *= 0
     # Рассматриваем каждый опыт работы
     good_exp = 0
     cur_positions = soup.find_all(attrs={"data-qa": "resume-block-experience-position"})
-    ind = 0
-    for pos in cur_positions:
-        if position in pos.get_text().lower():
-            exp = all_exp[ind]
+    i = 0
+    while i < (len(cur_positions)):
+        if position in cur_positions[i].get_text().lower():
+            # exp = all_exp[i]
             # print(exp.get_text())
             # получаем время
-            s = exp.get_text().split()
+            s = all_exp[i].get_text().split()
             if(len(s) == 4):
                 cur_exp = float(s[0])*12 + float(s[2])
             elif(s[1] == "год" or s[1] == "лет" or s[1] == "года"):
@@ -128,7 +129,7 @@ def parse_exp_in_resume(soup, demanded_exp, procents, position):
             # проверяем опыт
             if(cur_exp >= demanded_exp):
                 good_exp += cur_exp
-        ind += 1
+        i += 1
     if(good_exp/total_exp >= procents/100):
         return True
     else:
@@ -138,19 +139,24 @@ def parse_exp_in_resume(soup, demanded_exp, procents, position):
 def parse_resumes(links):
     print("Ищем подходящие резюме(фильтр по опыту)...")
     good_resumes = 0
-    links_len = len(links)
+    pbar = tqdm(total=len(links))
     with open("good_resumes.txt", 'w', encoding='utf-8') as f:
-        for link in tqdm(links):
+        i = 0
+        while i < len(links):
+            link = links[i]
             try:
-                html = get_html(link, False)
+                html = get_html(link)
                 soup = BeautifulSoup(html, 'lxml')
             except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
                 print(" Переподключение к страничке с резюме...")
-                time.sleep(3)
+                sleep(3)
             if(parse_exp_in_resume(soup, 24, 60, "менеджер")):  # !
                 f.write(link + '\n')
                 good_resumes += 1
+            i += 1
+            pbar.update()
     print("Найдено", good_resumes, "подходящих резюме.")
+    pbar.close()
 
 
 '''
@@ -158,7 +164,7 @@ def sort_relevant_jobs(keyword):  # сортирует резюме по кол-
     with open("good_resumes.txt", 'r', encoding='utf-8') as f:
         data = dict()
         for link in f:
-            html = get_html(link, False)
+            html = get_html(link)
             soup = BeautifulSoup(html, 'lxml')
             jobs = soup.find_all(attrs={"data-qa": "resume-block-experience-position"})
             ans = 0
@@ -176,18 +182,18 @@ def sort_relevant_jobs(keyword):  # сортирует резюме по кол-
 def apply_tags(progress: int):
     print("Проверяем теги первого и второго блока...")
     keywords11, keywords12, keywords13, keywords21 = [], [], [], []
+
     with open("1-1_condition.txt", 'r', encoding='utf-8') as f:
-        for word in f:
-            keywords11 += [word.strip()]
+        keywords11 = [word.strip() for word in f]
+
     with open("1-2_condition.txt", 'r', encoding='utf-8') as f:
-        for word in f:
-            keywords12 += [word.strip()]
+        keywords11 = [word.strip() for word in f]
+
     with open("1-3_condition.txt", 'r', encoding='utf-8') as f:
-        for word in f:
-            keywords13 += [word.strip()]
+        keywords11 = [word.strip() for word in f]
+
     with open("2-1_condition.txt", 'r', encoding='utf-8') as f:
-        for word in f:
-            keywords21 += [word.strip()]
+        keywords11 = [word.strip() for word in f]
 
     data = dict()
 
@@ -202,16 +208,17 @@ def apply_tags(progress: int):
     # test_dict21 = dict()
     # for k21 in keywords21:
     #     test_dict21[k21] = 0
-
+    pbar = tqdm(total=progress)
     with open("good_resumes.txt", 'r', encoding='utf-8') as f:
-        for link in tqdm(f, total=progress):
+        for link in (f):
+            pbar.update()
             total = 0
             try:
-                html = get_html(link, False)
+                html = get_html(link)
                 soup = BeautifulSoup(html, 'lxml')
             except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
                 print(" Переподключение к страничке с резюме...")
-                time.sleep(3)
+                sleep(3)
             job_dscrptn = soup.find_all(attrs={"data-qa": "resume-block-experience-description"})
             job = "".join(str(j) for j in job_dscrptn)
             job = job.replace("</div>", "")
@@ -227,19 +234,23 @@ def apply_tags(progress: int):
             # for k21 in keywords21:
             #     test_dict21[k21] += job.count(k21)
             total += 0.1*any(k21 in job for k21 in keywords21)
-            job = job.split('.')
+            job = tuple(job.split('.'))
             sentence_value = 0
             max_sentence_value = 0
-            for j in job:
+            i = 0
+            while i < len(job):
+                j = job[i]
                 sentence_value = (1 * any(k11 in j for k11 in keywords11) + 1 *
                                   any(k12 in j for k12 in keywords12) + 1 * any(k13 in j for k13 in keywords13))
                 if max_sentence_value < sentence_value:
                     max_sentence_value = sentence_value
                     if max_sentence_value == 3:
                         break
+                i += 1
             total += max_sentence_value
             if(total > 0.1):
                 data[link] = total
+    pbar.close()
     print("Найдено", len(data), "подходящих резюме.")
     with open("tagged_good_resumes.txt", 'w', encoding='utf-8') as f:
         for k in sorted(data, key=data.get, reverse=True):
@@ -264,7 +275,7 @@ if __name__ == '__main__':
     query = 'менеджер+по+продажам'
     area = '2'
     # сначала вытащим все ссылки на резюме по данному запросу и региону
-    links = get_all_resumes_links(query, area, 100)
+    links = get_all_resumes_links(query, area, 1000)
     # теперь распарсим информацию по каждой ссылке, полученной выше
     parse_resumes(links)  # по опыту работы
     print("Проверено", len(links), "вакансий.\n")
