78ddad14b1ee1ad748777275f633e88e7adabbc3
Egor <egorius@MacBook-Air-Egor.local>
2021-03-11 17:36:47 +0300

    v1.4 оценка по тегам

--- hiree.py
+++ hiree.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from time import sleep
 import os
 from tqdm import tqdm
+import subprocess
 # достает html код по указанной ссылке
 
 local_proc = 0
@@ -53,12 +54,7 @@ def get_resumes_links(html):
 def get_all_resumes(query, area, number_of_links):
     print("Собираем глобальные ссылки...")
     # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    # url_base = 'https://hh.ru/search/resume?clusters=True'
-    # url_area = '&area='+area
-    # url_base2 = '&order_by=relevance&logic=normal&pos=position&exp_period=all_time&no_magic=False&st=resumeSearch'
-    # url_text = '&text='+query
-    # url_page = '&page='
-
+    # https://hh.ru/search/resume?clusters=True&area=1&order_by=relevance&logic=normal&pos=position&exp_period=all_time&no_magic=False&st=resumeSearch&text=менеджер+по+продажам&page=0
     basic_url = "".join(tuple(("https://hh.ru/search/resume?clusters=True", "&area=", area,
                                "&order_by=relevance&logic=normal&pos=position&exp_period=all_time&no_magic=False&st=resumeSearch", "&text=", query, "&page=")))
     # когда не найдем с помощью bs4 нужный элемент, то выставим его False
@@ -99,7 +95,7 @@ def get_all_resumes(query, area, number_of_links):
 
 
 # Функция, которая парсит блок с опытом работы
-def parse_exp_in_resume(soup, demanded_exp, procents, position):
+def parse_exp_in_resume(soup, demanded_exp: int, procents: int, position):
     # находим общий опыт работы
     all_exp = soup.find_all('div', class_="bloko-text-tertiary")
     total_exp_str = soup.find(
@@ -119,37 +115,50 @@ def parse_exp_in_resume(soup, demanded_exp, procents, position):
         total_exp_value = int(total_exp_tuple[2])*12
     else:
         total_exp_value = int(total_exp_tuple[2])
-    # print("Общий стаж:", total_exp, "месяцев")
-    # cur_exp = 0  # Опыт на одном из мест
-    # s *= 0
+
     # Рассматриваем каждый опыт работы
     good_exp = 0
     cur_positions = soup.find_all(attrs={"data-qa": "resume-block-experience-position"})
     i = 0
+    # os.system("g++ exp.cpp -o exp")
     while i < (len(cur_positions)):
+        # os.system("./exp " + position + " " +
+        #           cur_positions[i].get_text().lower().partition(' ')[0] + " " +
+        #           '+'.join(all_exp[i].get_text().split()))
         if position in cur_positions[i].get_text().lower():
-            # exp = all_exp[i]
-            # print(exp.get_text())
-            # получаем время
             cur_exp_tuple = tuple(all_exp[i].get_text().split())
             if(len(cur_exp_tuple) == 4):
-                cur_exp = float(cur_exp_tuple[0])*12 + float(cur_exp_tuple[2])
+                cur_exp = int(cur_exp_tuple[0])*12 + int(cur_exp_tuple[2])
             elif(cur_exp_tuple[1] == "год" or cur_exp_tuple[1] == "лет" or cur_exp_tuple[1] == "года"):
-                cur_exp = float(cur_exp_tuple[0])*12
+                cur_exp = int(cur_exp_tuple[0])*12
             else:
-                cur_exp = float(cur_exp_tuple[0])
+                cur_exp = int(cur_exp_tuple[0])
             # проверяем опыт
             if(cur_exp >= demanded_exp):
                 good_exp += cur_exp
         i += 1
-    if(good_exp/total_exp_value >= procents/100):
+    if(float(good_exp)/total_exp_value >= procents/100):
         return True
     else:
         return False
 
 
-def experience_filter():
-    print("Применяем фильтр по опыту работы...")
+def demanded_sex(soup, sex: str):
+    personal_sex = soup.find(attrs={"data-qa": "resume-personal-gender"})
+    if personal_sex == None:
+        return True
+    return personal_sex.get_text().strip().lower() == sex
+
+
+def young_age(soup, age_limit: int):
+    age = soup.find(attrs={"data-qa": "resume-personal-age"})
+    if age == None:
+        return True
+    return int(age.get_text().strip().split()[0]) < age_limit
+
+
+def sexperience_filter():
+    print("Проверяем опыт работы...")
     good_resumes = 0
     with open("all_resumes.txt", 'r', encoding='utf-8') as read_file:
         with open("good_resumes.txt", 'w', encoding='utf-8') as write_file:
@@ -164,9 +173,17 @@ def experience_filter():
                 except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
                     print(" Переподключение к страничке с резюме...")
                     sleep(3)
-                if(parse_exp_in_resume(soup, 24, 60, "менеджер")):  # !
+                if not demanded_sex(soup, "мужчина"):
+                    i += 1
+                    pbar.update()
+                    continue
+                if young_age(soup, 26):
                     write_file.write(link+'\n')
                     good_resumes += 1
+                else:
+                    if(parse_exp_in_resume(soup, 24, 60, "менеджер")):  # !
+                        write_file.write(link+'\n')
+                        good_resumes += 1
                 i += 1
                 pbar.update()
     f = open("good_resumes.txt", "r")
@@ -218,7 +235,7 @@ def apply_tags():
 
     data = dict()
 
-    with open("good_resumes.txt", 'r', encoding='utf-8') as read_file:
+    with open("salary_resumes.txt", 'r', encoding='utf-8') as read_file:
         progress = int(read_file.readline().strip())
         link_ind = 0
         pbar = tqdm(total=progress)
@@ -241,24 +258,20 @@ def apply_tags():
             job = job.replace("<div data-qa=\"resume-block-experience-description\">", "")
             job = job.replace(';', '.')
 
-            mark = 0.1*any(k21 in job for k21 in keywords21)
+            total_value = 0.1*any(k21 in job for k21 in keywords21)
 
             job = job.split('.')
-            max_sentence_value = 0.0
             i = 0
             while i < len(job):
                 j = job[i]
                 sentence_value = 1 * any(k11 in j for k11 in keywords11)
                 sentence_value += 1 * any(k12 in j for k12 in keywords12)
                 sentence_value += 1 * any(k13 in j for k13 in keywords13)
-                if max_sentence_value < sentence_value:
-                    max_sentence_value = sentence_value
-                    if max_sentence_value == 3:
-                        break
+                if sentence_value > 0:
+                    total_value += sentence_value
                 i += 1
-            mark += max_sentence_value
-            if(mark > 0.1):
-                data[link] = mark
+            if(total_value > 1):
+                data[link] = total_value
             link_ind += 1
             pbar.update()
     pbar.close()
@@ -282,31 +295,32 @@ def get_zodiac(day_month: str):  # строка вида "день месяц" (
         astro_sign = "водолей" if (day < 19) else "рыба"
 
     elif month == "марта":
-        astro_sign = "рыба" if (day < 21) else astro_sign = "овен"
+        astro_sign = "рыба" if (day < 21) else "овен"
 
     elif month == "апреля":
-        astro_sign = "овен" if (day < 20) else astro_sign = "телец"
+        astro_sign = "овен" if (day < 20) else "телец"
 
     elif month == "мая":
-        astro_sign = "телец" if (day < 21) else astro_sign = "близнецы"
+        astro_sign = "телец" if (day < 21) else "близнецы"
 
     elif month == "июня":
-        astro_sign = "близнецы" if (day < 21) else astro_sign = "рак"
+        astro_sign = "близнецы" if (day < 21) else "рак"
 
     elif month == "июля":
-        astro_sign = "рак" if (day < 23) else astro_sign = "лев"
+        astro_sign = "рак" if (day < 23) else "лев"
 
     elif month == "августа":
-        astro_sign = "лев" if (day < 23) else astro_sign = "дева"
+        astro_sign = "лев" if (day < 23) else "дева"
 
     elif month == "сентября":
-        astro_sign = "дева" if (day < 23) else astro_sign = "весы"
+        astro_sign = "дева" if (day < 23) else "весы"
 
     elif month == "октября":
-        astro_sign = "весы" if (day < 23) else astro_sign = "скорпион"
+        astro_sign = "весы" if (day < 23) else "скорпион"
 
     elif month == "ноября":
-        astro_sign = "скорпион" if (day < 22) else astro_sign = "стрелец"
+        astro_sign = "скорпион" if (day < 22) else "стрелец"
+
     return astro_sign
 
 
@@ -314,7 +328,7 @@ def zodiac_filter(desired_sign: str):
     print("Проверяем знак зодиака...")
     # data = ()
     total = 0
-    with open("tagged_good_resumes.txt", 'r', encoding='utf-8') as read_file:
+    with open("good_resumes.txt", 'r', encoding='utf-8') as read_file:
         with open("zodiac_resumes.txt", 'w', encoding='utf-8') as write_file:
             progress = int(read_file.readline().strip())
             link_ind = 0
@@ -351,14 +365,66 @@ def zodiac_filter(desired_sign: str):
     print("Найденно", total, "подходящих резюме.")
 
 
+def salary_filter(min_salary: int, max_salary: int):
+    print("Проверяем желаемую зарплату...")
+    with open("zodiac_resumes.txt", 'r', encoding='utf-8') as read_file:
+        with open("salary_resumes.txt", 'w', encoding='utf-8') as write_file:
+            progress = int(read_file.readline().strip())
+            link_ind = 0
+            pbar = tqdm(total=progress)
+            total = 0
+            while link_ind < progress:
+                link = read_file.readline().strip()
+                try:
+                    html = get_html(link[:len(link)-4])
+                    soup = BeautifulSoup(html, 'lxml')
+                except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
+                    print(" Переподключение к страничке с резюме...")
+                    sleep(3)
+                mny = soup.find(
+                    'span', class_="resume-block__salary resume-block__title-text_salary")
+                if mny == None:
+                    write_file.write(link+'\n')
+                    total += 1
+                    link_ind += 1
+                    pbar.update()
+                    continue
+                mny = str(mny)
+                if not "руб." in mny:
+                    write_file.write(link+'\n')
+                    total += 1
+                    link_ind += 1
+                    pbar.update()
+                    continue
+                shit = "<span class=\"resume-block__salary resume-block__title-text_salary\" data-qa=\"resume-block-salary\">"
+                mny = mny.replace(shit, '')
+                mny = mny.replace("<!-- --> <!-- -->руб.</span>", '')
+                mny = mny.replace('\u2009', '')
+                mny = int(mny)
+                if mny > min_salary and mny < max_salary:
+                    write_file.write(link+'\n')
+                link_ind += 1
+                pbar.update()
+    pbar.close()
+    f = open("salary_resumes.txt", "r")
+    oline = f.readlines()
+    oline.insert(0, str(total)+'\n')
+    f.close()
+    f = open("salary_resumes.txt", "w")
+    f.writelines(oline)
+    f.close()
+    print("Найденно", total, "подходящих резюме.")
+
+
 if __name__ == '__main__':
     # print("Введите запрос:")
     # query = input().lower().replace(' ', '+')
-    # query = "менеджер+по+продажам"
-    # area = '2' # СПБ
-    # # сначала вытащим все ссылки на резюме по данному запросу и региону
-    # get_all_resumes(query, area, 5000)
-    # # теперь распарсим информацию по каждой ссылке, полученной выше
-    # experience_filter()  # по опыту работы
-    # apply_tags()
+    query = "менеджер+по+продажам"
+    area = '2'  # СПБ
+    # сначала вытащим все ссылки на резюме по данному запросу и региону
+    get_all_resumes(query, area, 1000)
+    # теперь распарсим информацию по каждой ссылке, полученной выше
+    sexperience_filter()  # по опыту работы и полу
     zodiac_filter("овен")
+    salary_filter(60000, 250000)
+    apply_tags()  # тэги
